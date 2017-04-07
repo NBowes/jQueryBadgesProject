081e10fd116dbef4478c22679d2940d118243778
NBowes <nathen.bowes@shopify.com>
2017-04-06 20:55:43 -0700

    codeschool changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,12 +10,13 @@ $(function() {
 
     function addCourse(courses){
         courses.forEach(function(course){
-          var $course = $('<div>').addClass('course').appendTo($badges);
-            $('<h3>').text(course.title).appendTo($course);
-            $('<img>').attr('src',course.badge).appendTo($course);
-            $('<a>').text("See Course").addClass('btn btn-primary')
-                                       .attr('href', course.url,
-                                              'target','_blank')
+          var $course = $('<div />',{'class':'course'}).appendTo($badges);
+            $('<h3 />',{text: course.title}).appendTo($course);
+            $('<img />',{src: course.badge}).appendTo($course);
+            $('<a />', {text: "See Course",
+                       'class':'btn btn-primary',
+                       'href' :course.url,
+                       'target':'_blank'})
                                        .appendTo($course);
         });
 
