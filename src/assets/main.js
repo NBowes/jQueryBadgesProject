2ff44e8ee1b6ca47ef97e26cbdf326f8c1539d77
NBowes <nathen.bowes@shopify.com>
2017-04-06 21:12:30 -0700

    fixed addCourse function

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,17 +6,16 @@ $(function() {
         addCourse(response.courses.completed);
       }
     });
-    var $badges = $('#badges');
 
     function addCourse(courses){
+      var $badges = $('#badges');
         courses.forEach(function(course){
-          var $course = $('<div />',{'class':'course'}).appendTo($badges);
-            $('<h3 />',{text: course.title}).appendTo($course);
-            $('<img />',{src: course.badge}).appendTo($course);
-            $('<a />', {text: "See Course",
-                       'class':'btn btn-primary',
-                       'href' :course.url,
-                       'target':'_blank'})
+          var $course = $('<div />').addClass('course').appendTo($badges);
+            $('<h3 />').text(course.title).appendTo($course);
+            $('<img />').attr('src',course.badge).appendTo($course);
+            $('<a />').text("See Course").addClass('btn btn-primary')
+                                       .attr('href', course.url,
+                                              'target','_blank')
                                        .appendTo($course);
         });
 
