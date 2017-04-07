d826135ed0a0f2e7738bf0e4ad6db44fb04dfef5
NBowes <nathen.bowes@shopify.com>
2017-04-06 20:38:28 -0700

    added images and buttons to courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,6 +3,7 @@ $(function() {
       url: 'https://www.codeschool.com/users/NBowes.json',
       dataType: 'jsonp',
       success: function(response){
+        console.log(response.courses.completed);
         addCourse(response.courses.completed);
       }
     });
@@ -10,7 +11,14 @@ $(function() {
 
     function addCourse(courses){
         courses.forEach(function(course){
-          $('<div>').addClass('course').appendTo($badges);
+          var $course = $('<div>').addClass('course').appendTo($badges);
+            $('<h3>').text(course.title).appendTo($course);
+            $('<img>').attr('src',course.badge).appendTo($course);
+            $('<a>').text("See Course").addClass('btn btn-primary')
+                                       .attr('href', course.url,
+                                              'target','_blank')
+                                       .appendTo($course);
         });
+
     }
 });
